d5114cef2239d62c9f487d3f95521829509154d2
soniyj <mark.punk182@gmail.com>
2018-10-23 11:11:39 +0200

    Oother Cocos2dx projects

---
+++ src/Cocos2d-x/TestGame1/cocos2d/cocos/platform/android/java/.d
@@ -0,0 +1,4 @@
+/Users/soniyj/soniyj/dev/workspace/CocosProjects/TestGame1/cocos2d/cocos/platform/android/java/gen/com/enhance/gameservice/IGameTuningService.java : \
+  /Users/soniyj/soniyj/dev/workspace/CocosProjects/TestGame1/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl
+
+/Users/soniyj/soniyj/dev/workspace/CocosProjects/TestGame1/cocos2d/cocos/platform/android/java/src/com/enhance/gameservice/IGameTuningService.aidl :
